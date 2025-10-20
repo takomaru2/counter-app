df3b28713c6c4f827500bf98424d5c2aaf7a54f9
takomaru2 <col.dd93@gmail.com>
2025-10-20 16:40:42 +0900

    feat: +1ボタン作成と機能実装

--- src/apps/counterApp/components/CounterApp.jsx
+++ src/apps/counterApp/components/CounterApp.jsx
@@ -1,3 +1,17 @@
+import styles from "./CounterApp.module.scss";
+import { useState } from "react";
+
 export const CounterApp = () => {
-  return <div>counter</div>;
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <div className={styles.counter}>{count}</div>
+      <button onClick={handleClick} className={styles.button}>
+        +1
+      </button>
+    </div>
+  );
 };

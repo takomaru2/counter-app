26330cbffe0716f688d878e8ac0604775cdc3d55
takomaru2 <col.dd93@gmail.com>
2025-10-20 16:58:18 +0900

    feat: -とxボタンも実装

--- src/apps/counterApp/components/CounterApp.jsx
+++ src/apps/counterApp/components/CounterApp.jsx
@@ -3,14 +3,31 @@ import { useState } from "react";
 
 export const CounterApp = () => {
   const [count, setCount] = useState(0);
-  function handleClick() {
+
+  const handleClick = () => {
     setCount(count + 1);
-  }
+  };
+  const handleClick1 = () => {
+    setCount(count - 1);
+  };
+  const handleClick2 = () => {
+    setCount(count * 2);
+  };
+
   return (
     <div>
       <div className={styles.counter}>{count}</div>
       <button onClick={handleClick} className={styles.button}>
         +1
+      </button>
+      <button onClick={handleClick} className={styles.button}>
+        +2
+      </button>
+      <button onClick={handleClick1} className={styles.button}>
+        -1
+      </button>
+      <button onClick={handleClick2} className={styles.button}>
+        x2
       </button>
     </div>
   );

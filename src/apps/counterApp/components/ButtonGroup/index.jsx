b7b91c69b14b48527c7904bac37b6dd23006d0a9
takomaru2 <col.dd93@gmail.com>
2025-10-21 22:54:49 +0900

    refactor: ButtonGroupコンポーネントを作成してボタンのmap処理をまとめる

---
+++ src/apps/counterApp/components/ButtonGroup/index.jsx
@@ -0,0 +1,14 @@
+export const ButtonGroup = ({ ButtonComponent, numbers, count, setCount }) => {
+  return (
+    <div>
+      {numbers.map((num) => (
+        <ButtonComponent
+          key={num}
+          num={num}
+          count={count}
+          setCount={setCount}
+        />
+      ))}
+    </div>
+  );
+};
--- src/apps/counterApp/components/CounterApp/index.jsx
+++ src/apps/counterApp/components/CounterApp/index.jsx
@@ -4,31 +4,45 @@ import { AddButton } from "../AddButton/index.jsx";
 import { MinusButton } from "../MinusButton/index.jsx";
 import { MultiplyButton } from "../MultiplyButton/index.jsx";
 import { generateNumbers } from "../../utils/generateNumbers.js";
+import { ButtonGroup } from "../ButtonGroup/index.jsx";
 
 export const CounterApp = () => {
   const [count, setCount] = useState(0);
 
   const numbers = generateNumbers();
 
-  const addButtons = numbers.map((num) => (
-    <AddButton num={num} count={count} setCount={setCount} />
-  ));
-
-  const minusButtons = numbers.map((num) => (
-    <MinusButton num={num} count={count} setCount={setCount} />
-  ));
-
-  const multiplyButtons = numbers.map((num) => {
-    if (num === 1) return;
-    return <MultiplyButton num={num} count={count} setCount={setCount} />;
-  });
-
   return (
     <div>
-      <div className={styles.counter}>{count}</div>
-      {addButtons}
-      {minusButtons}
-      {multiplyButtons}
+      <div className={styles.buttonWrapper}>
+        <div className={styles.buttonFlex}>
+          <ButtonGroup
+            ButtonComponent={AddButton}
+            count={count}
+            setCount={setCount}
+            numbers={numbers}
+          />
+          <ButtonGroup
+            ButtonComponent={MinusButton}
+            count={count}
+            setCount={setCount}
+            numbers={numbers}
+          />
+          <ButtonGroup
+            ButtonComponent={MultiplyButton}
+            count={count}
+            setCount={setCount}
+            numbers={numbers}
+          />
+        </div>
+      </div>
+      <div className={styles.boardWrapper}>
+        <img
+          src="/counterAppImage/bsk.JPG"
+          alt="ビスケと言う女性の画像"
+          className={styles.board}
+        />
+        <div className={styles.counter}>{count}</div>
+      </div>
     </div>
   );
 };

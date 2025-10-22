c6b03dd2f0df4fdbdaaa343f50d43a18c3bec011
takomaru2 <col.dd93@gmail.com>
2025-10-22 16:38:54 +0900

    feat: 予定外の文字列が入ってきた時のハンドリング

--- src/apps/counterApp/components/Button/index.jsx
+++ src/apps/counterApp/components/Button/index.jsx
@@ -3,18 +3,24 @@ import styles from "./index.module.scss";
 export const Button = ({ num, handleClick, operator }) => {
   let className;
 
+  //operartorが+か-かx以外だったら
+  const isTarget = operator === "+" || operator === "-" || operator === "x";
+  if (!isTarget) {
+    // if (operator !== "+" && operator !== "-" && operator !== "x") {
+    className = styles.add;
+  }
   if (operator === "+") {
-    className = styles.addButton;
+    className = styles.add;
   }
   if (operator === "-") {
-    className = styles.minusButton;
+    className = styles.minus;
   }
   if (operator === "x") {
-    className = styles.multiplyButton;
+    className = styles.multiply;
   }
 
   return (
-    <button onClick={handleClick} className={className}>
+    <button onClick={handleClick} className={`${styles.button} ${className}`}>
       {operator}
       {num}
     </button>

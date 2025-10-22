7a4167782d405579c9925032dd038ab72a39647c
takomaru2 <col.dd93@gmail.com>
2025-10-22 18:05:36 +0900

    refactor: エラー考慮をswitch文で書き換えた

--- src/apps/counterApp/components/Button/index.jsx
+++ src/apps/counterApp/components/Button/index.jsx
@@ -3,20 +3,19 @@ import styles from "./index.module.scss";
 export const Button = ({ num, handleClick, operator }) => {
   let className;
 
-  //operartorが+か-かx以外だったら
-  const isTarget = operator === "+" || operator === "-" || operator === "x";
-  if (!isTarget) {
-    // if (operator !== "+" && operator !== "-" && operator !== "x") {
-    className = styles.add;
-  }
-  if (operator === "+") {
-    className = styles.add;
-  }
-  if (operator === "-") {
-    className = styles.minus;
-  }
-  if (operator === "x") {
-    className = styles.multiply;
+  switch (operator) {
+    case "+":
+      className = styles.add;
+      break;
+    case "-":
+      className = styles.minus;
+      break;
+    case "x":
+      className = styles.multiply;
+      break;
+    default:
+      className = styles.add;
+      break;
   }
 
   return (

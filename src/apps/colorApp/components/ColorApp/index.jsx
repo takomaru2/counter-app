20c70a7128451e45053c67f3f871654d22bb4466
takomaru2 <col.dd93@gmail.com>
2025-10-23 17:46:53 +0900

    refactor: mapを使用してファイルも切り出した

--- src/apps/colorApp/components/ColorApp/index.jsx
+++ src/apps/colorApp/components/ColorApp/index.jsx
@@ -1,25 +1,14 @@
 import { Button } from "../Button/index.jsx";
 import styles from "./index.module.scss";
 import { useState } from "react";
+import { colors } from "../utils/colors.js";
 
 export const ColorApp = () => {
   const [borderColor, setBorderColor] = useState("red");
   const handleClick = (clickedColor) => {
     //押したらborderがその色に変わる。新しいクラスつける
-    // className = styles.border;
     setBorderColor(clickedColor);
   };
-  const colors = [
-    { name: "red", code: "#E74C3C" },
-    { name: "blue", code: "#3498DB" },
-    { name: "yellow", code: "#F1C40F" },
-    { name: "purple", code: "#9B59B6" },
-    { name: "green", code: "#2ECC71" },
-    { name: "orange", code: "#E67E22" },
-    { name: "pink", code: "#FF69B4" },
-    { name: "gray", code: "#95A5A6" },
-    { name: "cyan", code: "#1ABC9C" },
-  ];
 
   return (
     <div className={`${styles.wrapper} ${styles[borderColor]}`}>
@@ -31,19 +20,6 @@ export const ColorApp = () => {
           handleClick={handleClick}
         />
       ))}
-
-      {/*<Button color={"red"} colorCode={"#FF5733"} handleClick={handleClick} />*/}
-      {/*<Button color={"blue"} colorCode={"#3357FF"} handleClick={handleClick} />*/}
-      {/*<Button*/}
-      {/*  color={"purple"}*/}
-      {/*  colorCode={"#8e44ad"}*/}
-      {/*  handleClick={handleClick}*/}
-      {/*/>*/}
-      {/*<Button*/}
-      {/*  color={"yellow"}*/}
-      {/*  colorCode={"#F1C40F"}*/}
-      {/*  handleClick={handleClick}*/}
-      {/*/>*/}
     </div>
   );
 };
